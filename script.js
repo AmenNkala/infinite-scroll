63544c9a1999cc3fcb008005bcced941bc645dd2
AmenNkala <amennkala@gmail.com>
2023-04-10 14:40:26 +0200

    inf-scroll done

--- script.js
+++ script.js
@@ -1,13 +1,64 @@
-const count = 10;
-const apiKey = 'weZFUmB6Po0gIwzag7MQqKuZdytidSBbxK57MGwWWKo';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=YOUR_ACCESS_KEY&count=${count}`;
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+let imagesCount = 6;
+const apiKey = process.env.API_KEY;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${imagesCount}`;
+
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        imagesCount = 15;
+        apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${imagesCount}`;
+    }
+}
+
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    photosArray.forEach((photo) => {
+        const item = document.createElement("a");
+        setAttributes(item, { href: photo.links.html, target: "_blank" });
+
+        const img = document.createElement("img");
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        img.addEventListener('load', imageLoaded)
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
 
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-
-    }
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) { }
 }
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+})
+
+getPhotos();
